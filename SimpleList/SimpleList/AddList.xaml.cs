ac8acb4ac59c4c22df9a8427d57d56b8a455ba55
Nicholas <34730949+MercuryQ@users.noreply.github.com>
2018-04-21 11:36:01 -0700

    Program Files

---
+++ SimpleList/SimpleList/AddList.xaml.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleList
+{
+    /// <summary>
+    /// Interaction logic for AddList.xaml
+    /// </summary>
+    public partial class AddList : Window
+    {
+        public AddList()
+        {
+            InitializeComponent();
+           
+        }
+
+        
+        // declare HashSet to prevent duplications
+        HashSet<string> partyN = new HashSet<string>();
+       
+        // return the user to the MainWindow
+        private void GoHome_Click(object sender, RoutedEventArgs e)
+        {
+           
+            this.Hide();
+            MainWindow form1 = new MainWindow();
+            form1.ShowDialog();
+        }
+
+        // add names
+        private void AddPerson_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            // prevent empty input from being added to HashSet 
+            if (String.IsNullOrEmpty(this.addName.Text))
+            {
+                MessageBox.Show("Empty input! Please try again."); 
+            }
+            else
+            {
+                partyN.Add(addName.Text);   // add name to HashSet
+                addName.Clear(); // clear text box
+            }
+            
+            }
+
+        // show names
+        private void ShowPeople_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> allNames = partyN.ToList<string>(); // convert HashSet index to List to display properly
+         
+            // display the names in listBox
+            for (int i = 0; i < allNames.Count; i++)
+            {
+                myNames.Items.Add(allNames.ElementAt(i));
+            }
+        }
+
+        
+        private void ResetList_Click(object sender, RoutedEventArgs e)
+        {
+            myNames.Items.Clear(); // clears the display of the list
+        }
+
+        private void RemoveLastG_Click(object sender, RoutedEventArgs e)
+        {
+            partyN.Clear(); // clear data from HashSet
+            myNames.Items.Clear(); // clear display of the list
+        }
+    }
+
+       
+    }
+
---
+++ SimpleList/SimpleList/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        // exit program
+        private void CloseMe_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            
+        }
+        // override to shutdown program
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Application.Current.Shutdown();
+        }
+
+        // start list
+        private void MakeGList_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide(); // hide MainWindow
+            AddList form2 = new AddList();
+            form2.ShowDialog(); // direct user to AddList form
+
+        }
+
+      
+    }
+}
